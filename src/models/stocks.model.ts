bb8780cfa78754ee17f9757c504514fd85ffc6bd
0xzre <alilo.ghazali@gmail.com>
2024-07-12 18:43:18 +0700

    FIX: Fix makro

--- src/models/stocks.model.ts
+++ src/models/stocks.model.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose, { Document, Model, mongo, Schema } from "mongoose";
 
 export interface IStocks extends Document {
@@ -55,36 +54,6 @@ class StockModel {
     const model = mongoose.model<IStocks>("Stock", StockSchema);
     this.mongooseModel = model;
   }
-=======
-import mongoose from 'mongoose';
-
-
-class StockModel {
-    mongooseModel: any
-
-    constructor() {
-
-        const FundamentalSchema = new mongoose.Schema({
-            net_profit: { type: Number, required: true },
-            eps: { type: Number, required: true },
-            pbv: { type: Number, required: true },
-            roe: { type: Number, required: true },
-            equity: { type: Number, required: true }
-        }, { _id: false });
-        
-        const StockSchema = new mongoose.Schema({
-            code: { type: String, required: true, unique: true },
-            name: { type: String, required: true },
-            type: { type: String, required: true },
-            fundamental: { type: FundamentalSchema, required: true }
-        }, { timestamps: true });
-
-        const model = mongoose.model("Stock", StockSchema);
-        this.mongooseModel = model;
-    }
-
-    
->>>>>>> cda0c5f33602c21608a20e5cfe7f0ae73b145b20
 }
 
 export default new StockModel();
